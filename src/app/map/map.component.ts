3960c40a792bb78ff104b590ba228ad7ece4c1c3
Andrea <>
2018-12-06 21:17:48 +0100

    add geojson first test

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -6,118 +6,30 @@ import {MapService} from '../map.service';
 declare let L;
 
 @Component({
-    selector: 'app-map',
-    templateUrl: './map.component.html',
-    styleUrls: ['./map.component.css']
+  selector: 'app-map',
+  templateUrl: './map.component.html',
+  styleUrls: ['./map.component.css']
 })
 export class MapComponent implements OnInit {
-    op_name = 'Fazione 3: UFSTG';
-    op_text = '';
-    map;
-    drawControl;
-    public _opened = true;
 
-    constructor(private   mapService: MapService) {
+  constructor(private  mapService: MapService) {
+  }
 
-    }
-
-    ngOnInit() {
-        this.mapService.initializeMap();
-
-        /*
-         this._opened = true;
-        const mapbox_token = 'pk.eyJ1IjoiYWRyaWFubzEiLCJhIjoiY2loZjNldXQxMDA1eXUybTRjcHg1NmpvdSJ9._XP1KOZt9b6lerXf8tG6pw';
-
-
-        this.map = L.map('mapDiv').setView([44.857534, 7.355711], 17);
-        const osm = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png');
-
-        this.addGrid(this.map);
-
-        // selezione multi tiles
-        const tf = L.tileLayer('https://tile.thunderforest.com/outdoors/{z}/{x}/{y}.png?apikey=9e8117bb902147d6863cf0c1af2b32bd');
-        const mapboxSat = L.tileLayer('https://api.mapbox.com/v4/adriano1.o8n8cj41/{z}/{x}/{y}.png?access_token=' + mapbox_token);
-        const mapboxTerrain = L.tileLayer('https://api.mapbox.com/v4/adriano1.o8n9d1ca/{z}/{x}/{y}.png?access_token=' + mapbox_token);
-        this.map.addLayer(tf);
-        //prova
-
-        const drawnItems = L.featureGroup().addTo(this.map);
-        this.map.addControl(new L.Control.Layers({
-            'TF Outdoors': tf,
-            'OpenStreetMap': osm,
-            'Mapbox Sat': mapboxSat,
-            'Mapbox Terrain': mapboxTerrain
-        }, {'drawlayer': drawnItems}, {position: 'topleft'}));
-
-        var optionDraw = {
-            draw: {
-                polygon: true,
-                polyline: true,
-                rectangle: true,
-                circle: true,
-                marker: false
-            },
-            edit: {
-                featureGroup: drawnItems
-            }
-        };
-
-
-        this.drawControl = new L.Control.Draw(optionDraw);
-        this.map.addControl(this.drawControl);
-
-
-        this.map.on(L.Draw.Event.CREATED, function (event) {
-            const layer = event.layer;
-            drawnItems.addLayer(layer);
-        });
-
-
-        this.addCustomIcon(44.856337, 7.35798513581253, '/assets/Icons/point.png', 'start_3');
-        this.addCustomIcon(44.855354, 7.357810602843642, '/assets/Icons/point.png', 'Viet');
-        this.addCustomIcon(44.856861, 7.359311839848585, '/assets/Icons/point.png', 'cratere');
-        this.addCustomIcon(44.857307, 7.357830995918195, '/assets/Icons/point.png', 'americano');
-        this.addCustomIcon(44.857354, 7.356677935097191, '/assets/Icons/point.png', 'URP');
-        this.addCustomIcon(44.858479, 7.355405568261065, '/assets/Icons/point.png', 'ponte');
-        this.addCustomIcon(44.860117, 7.352257936671143, '/assets/Icons/point.png', 'IN/OUT');
+  ngOnInit() {
+    this.mapService.initializeMap();
+   }
+/*
+  setDrawingColor() {
+    this.drawControl.setDrawingOptions({
+      rectangle: {
+        shapeOptions: {
+          color: '#0000FF'
+        }
+      }
+    });
+  }
 
 */
-    }
-
-    setDrawingColor() {
-        this.drawControl.setDrawingOptions({
-            rectangle: {
-                shapeOptions: {
-                    color: '#0000FF'
-                }
-            }
-        });
-    }
-
-    addCustomIcon(lat, long, iconPath, label) {
-        var custom_icon = L.icon({
-            iconUrl: iconPath,
-            iconSize: [20, 20],
-            /*  iconAnchor: [22, 94],
-              popupAnchor: [-3, -76],
-              shadowSize: [68, 95],
-              shadowAnchor: [22, 94]*/
-            opacity: '0.5'
-        });
-
-        var marker = L.marker([lat, long], {draggable: false, icon: custom_icon});
-        marker.bindTooltip(label, {permanent: true, direction: 'bottom', offset: [0, 0]});
-        marker.addTo(this.map);
-    }
-
-
-    centerTo(lat, lon, zoom) {
-        this.map.flyTo([lat, lon], zoom);
-    }
-
-    getCenter() {
-        return this.map.getCenter();
-    }
 
 
 }
--- src/app/waypoint/waypoint.component.ts
+++ src/app/waypoint/waypoint.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {MapService} from "../map.service";
 
 @Component({
   selector: 'app-waypoint',
@@ -6,10 +7,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./waypoint.component.css']
 })
 export class WaypointComponent implements OnInit {
+  x =  this.mapService.getWaypointList();
 
-  constructor() { }
+
+  constructor(private mapService:MapService) { }
 
   ngOnInit() {
+    this.mapService.testGeoJson();
   }
+
+
+  addWaypoint(){
+   this.mapService.addWaypoint(44.860117, 7.352257936671143, '/assets/Icons/point.png', 'IN/OUT');
+
+  }
+
+   getCenter(){
+    return this.mapService.getCenter();
+   }
+
+   centerTo(lat,long,zoom){
+      this.mapService.centerTo(lat,long,zoom);
+   }
 
 }
